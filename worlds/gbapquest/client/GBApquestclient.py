a21a5ea46febd58e644681c36ea427d1d35f7c56
Hayumin <hayumigumi@gmail.com>
2026-07-29 19:59:52 -0600

    Receive Items

--- worlds/gbapquest/client/GBApquestclient.py
+++ worlds/gbapquest/client/GBApquestclient.py
@@ -3,12 +3,13 @@ from typing import TYPE_CHECKING
 
 from sympy import false
 
-from NetUtils import ClientStatus
+from NetUtils import ClientStatus, color
 
 from argparse import Namespace
 
 import worlds._bizhawk as bizhawk
 from worlds._bizhawk.client import BizHawkClient
+from ..rom import ItemNameToInventoryIndex
 
 
 if TYPE_CHECKING:
@@ -36,6 +37,7 @@ AddressDictionary={
     'HardMode':0x0300294e,
     'ExtraChest':0x0300294f,
     'HammerMode':0x03002950,
+    'ReceivedCount':0x03002951, # Unused Address
     '__data_end__':0x03002fd0,
 }
 
@@ -43,6 +45,7 @@ class GBAPquestClient(BizHawkClient):
     game = "GBAPQuest"
     system = "GBA"
     patch_suffix = ".apgbpq"
+    handledItems = 0
 
     async def validate_rom(self, ctx: "BizHawkClientContext") -> bool:
         try:
@@ -70,20 +73,22 @@ class GBAPquestClient(BizHawkClient):
             return
         
         try:
-            # Read save data
-            location_data = await bizhawk.read(
+            # Read data
+            location_data, LatestCountList, InventoryArray = await bizhawk.read(
                  ctx.bizhawk_ctx,
-                 [(AddressDictionary.get('g_CoordChecked',0x03001524), 0x0100, "System Bus")]
+                 [(AddressDictionary.get('g_CoordChecked',0x03001524), 0x0100, "System Bus"),
+                  (AddressDictionary.get('ReceivedCount',0x03002951), 0x01, "System Bus"),
+                  (AddressDictionary.get('Inventory',0x03002934),0x18,"System Bus")]
              )
             # Check Game Goal (0xb3 is the Boss Location)
-            if not ctx.finished_game and (location_data[0][0xB3]):
+            if not ctx.finished_game and (location_data[0xB3]):
                 await ctx.send_msgs([{
                     "cmd": "StatusUpdate",
                     "status": ClientStatus.CLIENT_GOAL
                 }])
             new_checks = []
             for LocationInt in ctx.missing_locations:
-                if (location_data[0][LocationInt]):
+                if (location_data[LocationInt]):
                     new_checks.append(LocationInt)
 
                       
@@ -94,6 +99,29 @@ class GBAPquestClient(BizHawkClient):
                             f'New Check: {location} ({len(ctx.locations_checked)}/'
                             f'{len(ctx.missing_locations) + len(ctx.checked_locations)})')
                         await ctx.send_msgs([{"cmd": 'LocationChecks', "locations": [new_check_id]}])
+            'ReceivedCount'
+            writes=[]
+            guards=[]
+            if self.handledItems < len(ctx.items_received):
+                item = ctx.items_received[self.handledItems]
+                logging.info('Received %s from %s (%s) (%d/%d in list)' % (
+                                color(ctx.item_names.lookup_in_slot(item.item), 'red', 'bold'),
+                                color(ctx.player_names[item.player], 'yellow'),
+                                ctx.location_names.lookup_in_slot(item.location, item.player), self.handledItems+1, len(ctx.items_received)))
+                Inventoryindex=ItemNameToInventoryIndex.get(ctx.item_names.lookup_in_slot(item.item), -1)
+                guards.append((AddressDictionary.get('Inventory',0x03002934)+Inventoryindex*0x04+2,InventoryArray[Inventoryindex*0x04+2],"System Bus"))
+                
+                if Inventoryindex!=-1:
+                    logging.info('At Address %s(%s+%s) Value %s', 
+                                                AddressDictionary.get('Inventory',0x03002934)+Inventoryindex*0x04+2,
+                                                AddressDictionary.get('Inventory',0x03002934),
+                                                Inventoryindex*0x04+2,
+                                                InventoryArray[Inventoryindex*0x04+2])
+                    writes.append((AddressDictionary.get('Inventory',0x03002934)+Inventoryindex*0x04+2,bytes([InventoryArray[Inventoryindex*0x04+2]+1]),"System Bus"))
+                writes.append((AddressDictionary.get('ReceivedCount',0x03002951), bytes([self.handledItems+1]), "System Bus"))
+                self.handledItems=self.handledItems+1
+            await bizhawk.write(ctx.bizhawk_ctx,writes)
+                
         except bizhawk.RequestFailedError:
             # The connector didn't respond. Exit handler and return to main loop to reconnect
             pass
